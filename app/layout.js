de472950c06e0fc9f2176389ed406804c735f69c
jamesbeeker007-cell <jamesbeeker007@gmail.com>
2026-09-26 08:15:48 -0500

    Trial face: Inter in place of Space Grotesk

--- app/layout.js
+++ app/layout.js
@@ -13,11 +13,11 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body className="min-h-screen bg-hive-indigo text-white antialiased font-space-grotesk">
+      <body className="min-h-screen bg-hive-indigo text-white antialiased font-sans">
         {children}
       </body>
     </html>
